b3fe9dec621a866efcfc7d543300f14418f84399
Andrei Sosnin <An.S-1@yandex.ru>
2024-12-27 23:04:54 +0300

    main 1

--- src/main/java/ru/practikum/numbers/exception/DoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package ru.practikum.numbers.exception;
-
-public class DoesNotExistException extends RuntimeException {
-    public DoesNotExistException(final String message) {
-        super(message);
-    }
-}
